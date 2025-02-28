a8c9c4417f5554490a56830e23aef03c65ace6ea
austinguin0709 <austinguin0709@gmail.com>
2025-02-27 20:20:58 -0500

    Removed basic setup of quasar to start fresh.

--- simple-sign-in/src/pages/IndexPage.vue
+++ simple-sign-in/src/pages/IndexPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex flex-center">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, doloremque odit a labore quidem natus minima explicabo repellat eum hic illum distinctio aliquam deserunt consequuntur rerum. Modi debitis maiores ducimus.
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup>
--- simple-sign-in/src/router/routes.js
+++ simple-sign-in/src/router/routes.js
@@ -1,7 +1,6 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
